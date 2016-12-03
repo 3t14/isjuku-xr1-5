59fad30610a7b1f94232f0bafb3c7a19d4fba61b
3t14 <michiru@r4hs.com>
2016-12-03 11:02:14 +0900

    if ... else の使い方とTryParseの使い方

--- Program.cs
+++ Program.cs
@@ -6,6 +6,18 @@ namespace xr1_5
 	{
 		public static void Main(string[] args)
 		{
+			// 選択ステートメント
+			Console.WriteLine("数字を入力してください。");
+			string valueStr = Console.ReadLine();
+			int result;
+			if (int.TryParse(valueStr, out result))
+			{
+				Console.WriteLine("入力された値は数字でした。: {0}", result);
+			}
+			else {
+				Console.WriteLine("入力あれた値は数字ではりませんでした。:{0}", result);
+			}
+
 
 		}
 	}
